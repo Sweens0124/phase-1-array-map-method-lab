733b9cf20aae0dcc2a6c9f37844f41ea5e5449e6
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-21 17:28:06 -0600

    commit tree 119f069cc2ddeebbffaf4c5c6c16c8713e1cddc4

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+//1) how to get first letter of each word in each element of array: split()
+//2) split will return a new array with each word seperated from orinal elmnt
+//3) how to capitalize each letter of each word in new arrays: charAt(index)and toUpperCase? .charAt(0).toUpperCase()??
+
+
+// const newTutorials = tutorials.map(function (word) {
+//   return word.split(' ')
+// })
+// console.log(newTutorials);
+// const capitalWords = newTutorials.map(word => word.charAt(0).toUpperCase() + word.slice(1)
+// )
+// console.log(capitalWords);
+//how do we JOIN all the string elements into one string: join()/??? 
+//join with (' ') ??
+
+
+
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    const newTutorials = tutorial.split(' ')
+    const capitalWords = newTutorials.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    return capitalWords.join(' ')
+  })
+
 }
+
+console.log(titleCased())
